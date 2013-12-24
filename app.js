f52312c6925dd3a398e65786342fdae02011e19b
WeeJeWel <emile@emile-desktop.local>
2013-12-24 19:26:35 +0100

    Playlist support, supports ':' in track titles

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var config = {
 }
 
 process.on('uncaughtException', function (err) {
+	
+	console.log(err.stack);
+	
 	if( err.toString().indexOf('Track is not playable in country') > -1 ) { // skip track
 	
 		console.log(err.toString() + ': ' + current_track);
@@ -21,7 +24,31 @@ process.on('uncaughtException', function (err) {
 	}
 });
 
-var modules_folder = __dirname + '/node_modules.' + require('os').platform() + '.' + require('os').arch() + '/';
+// generate modules folder, OS specific
+var modules_folder = __dirname + '/node_modules';
+if( require('os').platform() == 'win32' ) {
+	if( require('os').arch() == 'ia32' ) {
+		modules_folder = modules_folder + '.win32.ia32'
+	} else if( require('os').arch() == 'x64' ){
+		modules_folder = modules_folder + '.win32.x64'
+	}
+} else if( require('os').platform() == 'darwin' ) {
+	if( require('os').arch() == 'x64' ){
+		modules_folder = modules_folder + '.darwin.x64'
+	}
+}
+
+modules_folder = modules_folder + '/';
+
+if( modules_folder == __dirname + '/node_modules/' ) {
+	console.log('Unsupported OS + architecture. Please run the following commands manually and re-run:');
+	console.log('npm install async');
+	console.log('npm install express');
+	console.log('npm install socket.io');
+	console.log('npm install spotify-web');
+	console.log('With your architecture, the ID3 tagging may or may not work! Please be sure the command `id3tag` is working.');
+	
+}
 
 var fs = require('fs');
 var async = require(modules_folder + 'async');
@@ -34,6 +61,8 @@ var socket = socket;
 var current_track = {};
 var tracks = [];
 var tracks_done = [];
+var playlist_id = false;
+var playlist_folder = false;
 
 var waitForSpotify = function(){
 	console.log('waitForSpotify');
@@ -73,10 +102,13 @@ io.sockets.on('connection', function (socket_) {
 		socket.emit('logged-in');
 	}
 	
-	socket.on('go', function (result) {
+	socket.on('go', function (result) {	
 		current_track = {};
 		tracks = [];
 		tracks_done = [];
+		playlist_id = false;
+		playlist_folder = false;
+		
 		downloadTracks( result.tracks );		
 	});
 	socket.on('login', function (result, callback) {
@@ -96,34 +128,99 @@ io.sockets.on('connection', function (socket_) {
 
 var downloadTracks = function(tracks_) {
 	
-	tracks = tracks_;
+	// check if playlist. if so, fill the tracks object with the playlist contents
+	// only 1 playlist download at the time
+	if( tracks_[0].title.indexOf('/playlist/') > 0 ) {
+		var uri = tracks_[0].title;
+		uri = uri.replace('http://open.spotify.com/', 'spotify:');
+		uri = uri.replace(/\//g, ':');
+						
+		downloadPlaylist( uri );
+	} else {
+		tracks = tracks_;
+	}
 	
 	async.eachSeries(tracks, function (track, callback) {
 		downloadTrack( 'spotify:track:' + track.id, callback );
 	}, function (err) {
 		if (err) { console.log(err); }
 		console.log('All done!');
+		
+		if( playlist_id && playlist_folder ) {	
+			socket.emit('done', {
+				id: playlist_id,
+				filepath: __dirname + '/mp3/' + playlist_folder
+			});					
+		}
+		
 		tracks_done = [];
 	});	
 }
 
-var downloadTrack = function( uri, callback ){
+var downloadPlaylist = function( uri ) {
 		
 	// generate id
 	var id = uri.split(':');
-		id = id[ id.length-1 ];	
+    	id = id[ id.length-1 ];
+    	
+    playlist_id = id;
+	    	
+	spotify_web.login(config.username, config.password, function (err, spotify) {
+	    	
+		if (err) {
+			socket.emit('error', {
+				id: id
+			});
+			return false;
+		}
+		
+		spotify.playlist( uri, 0, 99999, function(err, result){
+			if (err) {
+				socket.emit('error', {
+					id: id
+				});
+				return false;
+			}
+			
+			playlist_folder = result.attributes.name;
+			
+			for( var i = 0; i < result.contents.items.length; i++ ) {
+				tracks.push({
+					title: '',
+					id: result.contents.items[i].uri.replace('spotify:track:', '')
+				});
+			}
+			
+			downloadTracks( tracks );
+		});
+	});
+}
+
+var downloadTrack = function( uri, callback ){
+				
+	// generate id
+	var id = uri.split(':');
+    	id = id[ id.length-1 ]; 
 	
 	current_track = id;
 	
 	socket.emit('busy', {
-		id: id
+		id: ( playlist_id ) ? playlist_id : id
 	});
 	
 	spotify_web.login(config.username, config.password, function (err, spotify) {
+		if (err) {
+			console.log(err);
+			socket.emit('error', {
+				id: id
+			});
+			return false;
+		}
 	
 		// first get a "Track" instance from the track URI
 		spotify.get(uri, function (err, track) {
 			if (err) {
+				console.log(err);
 				socket.emit('error', {
 					id: id
 				});
@@ -137,24 +234,35 @@ var downloadTrack = function( uri, callback ){
 			}
 			artists = artists.join(' / ');
 			
-			// generate the artist path
-			var artistpath = __dirname + '/mp3/' + track.artist[0].name.replace(/\//g, ' - ') + '/';
-							
-			// generate folder if it does not exist
-			if( !fs.existsSync(artistpath) ) {
-				fs.mkdir( artistpath );
-			}
 			
-			// generate the albumpath
-			var albumpath = artistpath + track.album.name.replace(/\//g, ' - ') + ' [' + track.album.date.year + ']/';
-							
-			// generate folder if it does not exist
-			if( !fs.existsSync(albumpath) ) {
-				fs.mkdir( albumpath );
-			}
+			if( playlist_folder ) {
+				var albumpath = __dirname + '/mp3/' + playlist_folder + '/';
+								
+				// generate folder if it does not exist
+				if( !fs.existsSync(albumpath) ) {
+					fs.mkdir( albumpath );
+				}
+				
+			} else {			
+				// generate the artist path
+				var artistpath = __dirname + '/mp3/' + fixTrackName(track.artist[0].name) + '/';
+								
+				// generate folder if it does not exist
+				if( !fs.existsSync(artistpath) ) {
+					fs.mkdir( artistpath );
+				}
+				
+				// generate the albumpath
+				var albumpath = artistpath + fixTrackName(track.album.name) + ' [' + track.album.date.year + ']/';
+								
+				// generate folder if it does not exist
+				if( !fs.existsSync(albumpath) ) {
+					fs.mkdir( albumpath );
+				}
+			}				
 			
 			// generate the filepath
-			var filepath = albumpath + track.artist[0].name.replace(/\//g, ' - ') + ' - ' + track.name.replace(/\//g, ' - ') + '.mp3';
+			var filepath = albumpath + fixTrackName(track.artist[0].name) + ' - ' + fixTrackName(track.name) + '.mp3';
 	
 			// create filestream for the .mp3
 			var out = fs.createWriteStream( filepath );
@@ -173,10 +281,12 @@ var downloadTrack = function( uri, callback ){
 				}
 				console.log('ID3\'d: %s - %s', track.artist[0].name, track.name);
 				
-				socket.emit('done', {
-					id: id,
-					filepath: filepath
-				});
+				if( !playlist_id ) {			
+					socket.emit('done', {
+						id: id,
+						filepath: filepath
+					});
+				}
 				
 				tracks_done.push( id );
 				
@@ -188,4 +298,12 @@ var downloadTrack = function( uri, callback ){
 	
 		});
 	});
+}
+
+var fixTrackName = function( input ) {
+	var output = input;
+		output = output.replace(/\//g, ' - ');
+		output = output.replace(/\:/g, ' - ');
+	
+	return output;
 }
